66e1ec96275b01d847e979f77441502349020f7d
springstudent2016 <13817191469@163.com>
2018-08-30 09:07:04 +0800

    完善

--- src/test/java/com/gwg/shiro/web/service/UserServiceTest.java
+++ src/test/java/com/gwg/shiro/web/service/UserServiceTest.java
@@ -1,24 +1,22 @@
 package com.gwg.shiro.web.service;
 
+import com.gwg.shiro.web.config.jdbc.DataSourceConfig;
 import com.gwg.shiro.web.config.mvc.RootConfig;
-import com.gwg.shiro.web.config.mvc.WebConfig;
 import com.gwg.shiro.web.dto.UserDto;
 import com.gwg.shiro.web.exception.BusinessException;
+import com.gwg.shiro.web.mapper.UserMapper;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.objectweb.asm.util.TraceClassVisitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.asm.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.io.*;
 import java.util.Date;
+
 
 public class UserServiceTest {
 
@@ -26,13 +24,33 @@ public class UserServiceTest {
 
     private UserService userService;
 
-    /**
-     * 每个测试方法执行之前执行一次
-     */
+    /* * 每个测试方法执行之前执行一次*/
     @Before
     public void before(){
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class);
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(RootConfig.class, DataSourceConfig.class);
         userService = applicationContext.getBean(UserService.class);
+        UserMapper userMapper = applicationContext.getBean(UserMapper.class);
+        try {
+            ClassReader cr = new ClassReader(UserMapper.class.getResourceAsStream("UserMapper.class"));
+            System.out.println(userMapper.getClass());
+            System.out.println(userMapper.getClass().getName());
+            ClassWriter cw = new ClassWriter(cr, 0);
+            byte[] bytes = cw.toByteArray();
+
+            OutputStream out = new FileOutputStream("E:/test.class");
+            InputStream is = new ByteArrayInputStream(bytes);
+            byte[] buff = new byte[1024];
+            int len = 0;
+            while((len=is.read(buff))!=-1){
+                out.write(buff, 0, len);
+            }
+            is.close();
+            out.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
