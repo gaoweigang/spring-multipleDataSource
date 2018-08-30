c6f5ca707a95aeb74cc3ec618efe8847ac66ff07
gaoweigang <13817191469@163.com>
2018-08-30 14:05:09 +0800

    完善

--- src/main/java/com/gwg/shiro/web/vo/CusMapVo.java
+++ src/main/java/com/gwg/shiro/web/vo/CusMapVo.java
@@ -3,7 +3,6 @@ package com.gwg.shiro.web.vo;
 import lombok.Data;
 
 import java.io.Serializable;
-@Data
 public class CusMapVo implements Serializable{
 
     //key
@@ -12,4 +11,19 @@ public class CusMapVo implements Serializable{
     //value
     private String cusValue;
 
+    public String getCusKey() {
+        return cusKey;
+    }
+
+    public void setCusKey(String cusKey) {
+        this.cusKey = cusKey;
+    }
+
+    public String getCusValue() {
+        return cusValue;
+    }
+
+    public void setCusValue(String cusValue) {
+        this.cusValue = cusValue;
+    }
 }
